7585261dd1b1c6c99dada5d2d1aabf482e89e880
Kyuhong Shim <skhu20@snu.ac.kr>
2017-02-22 18:52:11 +0900

    add 0222

--- lemontree/layers/merge.py
+++ lemontree/layers/merge.py
@@ -13,23 +13,12 @@ class MergeAddLayer(BaseLayer):
     """
     This class implements layer add.
     """
-    def __init__(self, input_shape,):
+    def __init__(self):
         """
         This function initializes the class.
         The shape of two tensor should be equal.
-
-        Parameters
-        ----------
-        input_shape: tuple
-            a tuple of three values, i.e., (input channel, input width, input height).
-        output_shape: tuple
-            a tuple of single value, i.e., (input channel,) or (input dim,).
         """
         super(MergeAddLayer, self).__init__()
-        # check asserts
-        assert isinstance(input_shape, tuple), '"input_shape" should be a tuple.'
-        # set members
-        self.input_shape = input_shape
 
     def get_output(self, input1_, input2_):
         """
@@ -45,4 +34,32 @@ class MergeAddLayer(BaseLayer):
         TensorVariable
         """
         return input1_ + input2_
+
+
+class MergeConcatenateLayer(BaseLayer):
+    """
+    This class implements layer concatenation.
+    """
+    def __init__(self, axis=0):
+        """
+        This function initializes the class.
+        The shape of two tensor should be equal except given axis.
+        """
+        super(MergeConcatenateLayer, self).__init__()
+        self.axis = axis
+
+    def get_output(self, input1_, input2_):
+        """
+        This function overrides the parents' one.
+        Creates symbolic function to compute output from an input.
+
+        Parameters
+        ----------
+        input_: TensorVariable
+
+        Returns
+        -------
+        TensorVariable
+        """
+        return T.concatenate([input1_, input2_], axis=self.axis)
 
--- lemontree/layers/variational.py
+++ lemontree/layers/variational.py
@@ -22,7 +22,7 @@ class Latent1DLayer(BaseLayer):
         Parameters
         ----------
         input_shape: tuple
-            a tuple of three values, i.e., (input channel, input width, input height).
+            a tuple of single value, i.e., (input dim,).
         output_shape: tuple
             a tuple of single value, i.e., (input channel,) or (input dim,).
         seed: int
@@ -60,7 +60,7 @@ class GaussianNoise1DLayer(BaseLayer):
     """
     This class implements adding Gaussian noise to input.
     """
-    def __init__(self, input_shape, output_shape, seed=256567, sigma=1.0):
+    def __init__(self, input_shape,  sigma=1.0, seed=256567):
         """
         This function initializes the class.
         The shape of two tensor should be double.
@@ -68,22 +68,17 @@ class GaussianNoise1DLayer(BaseLayer):
         Parameters
         ----------
         input_shape: tuple
-            a tuple of three values, i.e., (input channel, input width, input height).
-        output_shape: tuple
-            a tuple of single value, i.e., (input channel,) or (input dim,).
+            a tuple of single value, i.e., (input dim,).
         seed: int
             an integer for random seed.
         sigma: float, default: 1.0
             a float value that will be sigma (std) of Gaussian noise.
         """
-        super(Latent1DLayer, self).__init__()
+        super(GaussianNoise1DLayer, self).__init__()
         # check asserts
         assert isinstance(input_shape, tuple) and len(input_shape) == 1, '"input_shape" should be a tuple.'
-        assert isinstance(output_shape, tuple) and len(output_shape) == 1, '"output_shape" should be a tuple.'
-        assert output_shape[0] * 2 == input_shape[0], '"output_shape" is half of "input_shape", since it contains mu and logvar.'
         # set members
         self.input_shape = input_shape
-        self.output_shape = output_shape
         self.rng = MRG(seed)
         self.sigma = sigma
 
@@ -100,5 +95,47 @@ class GaussianNoise1DLayer(BaseLayer):
         -------
         TensorVariable
         """
-        return input_ + self.rng.normal((self.batch_size, self.output_shape[0]), 0, self.sigma)
+        return input_ + self.rng.normal((self.batch_size, self.input_shape[0]), 0, self.sigma)
+
+
+class GaussianNoise3DLayer(BaseLayer):
+    """
+    This class implements adding Gaussian noise to input.
+    """
+    def __init__(self, input_shape,  sigma=1.0, seed=256567):
+        """
+        This function initializes the class.
+        The shape of two tensor should be double.
+
+        Parameters
+        ----------
+        input_shape: tuple
+            a tuple of three value, i.e., (channel, width, height).
+        seed: int
+            an integer for random seed.
+        sigma: float, default: 1.0
+            a float value that will be sigma (std) of Gaussian noise.
+        """
+        super(GaussianNoise3DLayer, self).__init__()
+        # check asserts
+        assert isinstance(input_shape, tuple) and len(input_shape) == 3, '"input_shape" should be a tuple of three values.'
+        # set members
+        self.input_shape = input_shape
+        self.rng = MRG(seed)
+        self.sigma = sigma
+
+    def get_output(self, input_):
+        """
+        This function overrides the parents' one.
+        Creates symbolic function to compute output from an input.
+
+        Parameters
+        ----------
+        input_: TensorVariable
+
+        Returns
+        -------
+        TensorVariable
+        """
+        return input_ + self.rng.normal((self.batch_size, self.input_shape[0], self.input_shape[1], self.input_shape[2]), 0, self.sigma)
 
